e38fe8a2d4cb2f340ed4734fb11029eb7a012a4f
Natanael <natanael.rabello@outlook.com>
2019-08-28 21:42:29 -0300

    Parameterized tests for ChangeStatus

--- test/capnp/changes.cc
+++ test/capnp/changes.cc
@@ -4,27 +4,42 @@
  **/
 
 #include "gtest/gtest.h"
-#include <string_view>
+#include <string>
+#include <tuple>
 
-#include "fmt/core.h"
+#include "fmt/format.h"
 #include "ger/changes.capnp.h"
 #include "capnp/compat/json.h"
 
-TEST(JsonCodec, ChangeStatus)
+class ChangeStatusTest
+    : public ::testing::TestWithParam<std::pair<gerrit::ChangeStatus, std::string_view>> {
+};
+
+INSTANTIATE_TEST_CASE_P(
+    JsonCodec, ChangeStatusTest,
+    ::testing::Values(std::pair{ gerrit::ChangeStatus::NEW, "NEW" },
+                      std::pair{ gerrit::ChangeStatus::MERGED, "MERGED" },
+                      std::pair{ gerrit::ChangeStatus::ABANDONED, "ABANDONED" },
+                      std::pair{ gerrit::ChangeStatus::DRAFT, "DRAFT" }));
+
+TEST_P(ChangeStatusTest, EncodeDecode)
 {
-    constexpr std::string_view input = R"({"status":"MERGED"})";
+    const gerrit::ChangeStatus kStatus = GetParam().first;
+    const std::string_view kStatusName = GetParam().second;
+
     capnp::MallocMessageBuilder arena;
     capnp::JsonCodec codec;
     codec.handleByAnnotation<gerrit::ChangeStatus>();
 
-    /* Decode ChangeStatus from json */
-    auto orphan = codec.decode<gerrit::ChangeTest>({ input.begin(), input.end() },
+    /* Decode from JSON to Enum */
+    const std::string in_json = fmt::format(R"({{"status":"{}"}})", kStatusName);
+    auto orphan = codec.decode<gerrit::ChangeTest>({ in_json.data(), in_json.length() },
                                                    arena.getOrphanage());
-    EXPECT_EQ(gerrit::ChangeStatus::MERGED, orphan.getReader().getStatus());
+    EXPECT_EQ(kStatus, orphan.getReader().getStatus());
 
-    /* Encode ChangeStatus to json */
+    /* Encode Enum to JSON */
     auto builder = arena.initRoot<gerrit::ChangeTest>();
-    builder.setStatus(gerrit::ChangeStatus::DRAFT);
-    auto output = codec.encode(builder);
-    EXPECT_STREQ(R"({"status":"DRAFT"})", output.cStr());
+    builder.setStatus(kStatus);
+    auto out_json = codec.encode(builder);
+    EXPECT_STREQ(fmt::format(R"({{"status":"{}"}})", kStatusName).c_str(), out_json.cStr());
 }
